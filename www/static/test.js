7788310387e8b2ff48815802c562edf43f6687da
Akand Sabbir Ahmed <bitsway@gmail.com>
2014-04-15 16:16:16 +0600

    4th commit

--- www/static/test.js
+++ www/static/test.js
@@ -2,7 +2,7 @@
 //var apipath='http://e.businesssolutionapps.com/panicbutton/default_with_sync_code/';
 var apipath='http://127.0.0.1:8000/unilever/default/';
 
-
+var imagePath = '';
 
 // ====================================Camera==========
 
@@ -15,8 +15,40 @@ function get_pic() {
 function onSuccess(imageURI) {
     var image = document.getElementById('myImage');
     image.src = imageURI;
+	imagePath = imageURI;
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+
+//==============================Upload Pic ==============
+function uploadPhoto() {
+	var imageURI = imagePath;
+    var options = new FileUploadOptions();
+    options.fileKey="upload";
+    options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+    options.mimeType="image/jpeg";
+
+    var params = {};
+    params.value1 = "test";
+    params.value2 = "param";
+
+    options.params = params;
+
+    var ft = new FileTransfer();
+    ft.upload(imageURI, encodeURI("http://e2.businesssolutionapps.com/welcome/test/fileUploader/"),win,fail,options);
+	
+}
+
+function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+}
+
+function fail(error) {
+    alert("An error has occurred: Code = " + error.code);
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
 }
